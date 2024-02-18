62e8de3adc59cfdd3a1254a19e5cff1e91c909b4
Shay Snyder <snyderse@outlook.com>
2024-02-17 20:26:54 -0500

    adding intro print and updating imports

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import argparse
 from omegaconf import DictConfig
 import os
 
-from test_functions import function_factory
+from test_functions import function_factory, VALID_FUNCTIONS
 
 DESCRIPTION = "Hyperdimensional Bayesian Optimization in Lava"
 
@@ -12,6 +12,14 @@ def get_config() -> DictConfig:
     """
     parser = argparse.ArgumentParser(description=DESCRIPTION)
 
+    parser.add_argument(
+        "--function",
+        type=str,
+        choices=VALID_FUNCTIONS,
+        default="branin",
+        help="The function to optimize",
+    )
+
     args = parser.parse_args()
 
     config: dict = vars(args)
@@ -20,11 +28,23 @@ def get_config() -> DictConfig:
     return config
 
 
-def main():
+def print_intro(config, delimiter="-", delimiter_width=60):
     """
     TODO Finish Documentation
     """
-    pass
+    print(delimiter * delimiter_width)
+    print(DESCRIPTION)
+    print(delimiter * delimiter_width)
+    print(f"Configuration:")
+    print(f" - Black Box Function: {config.function}")
+
+
+def main(config: DictConfig):
+    """
+    TODO Finish Documentation
+    """
+    print_intro(config)
+
 
 if __name__ == "__main__":
     config = get_config()
