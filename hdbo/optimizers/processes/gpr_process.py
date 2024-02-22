7dae2f82a67bdc78fa50917eb20583e79d9ef89d
Shay Snyder <snyderse@outlook.com>
2024-02-21 21:24:01 -0500

    Add GPOptimizerProcess and VSAOptimizerProcess classes

---
+++ hdbo/optimizers/processes/gpr_process.py
@@ -0,0 +1,29 @@
+from .base_optimizer import BaseOptimizerProcess
+
+class GPOptimizerProcess(BaseOptimizerProcess):
+    """
+    Represents a process for the GP optimizer.
+
+    This class provides an implementation for the GP optimizer process.
+
+    Attributes:
+        num_params (int): The number of parameters.
+        num_repeats (int): The number of repeats.
+        num_outputs (int): The number of outputs.
+        input (InPort): The input port.
+        output (OutPort): The output port.
+    """
+
+    def __init__(self, num_params: int, num_repeats: int = 1,
+                 num_outputs: int = 1, **kwargs):
+        """
+        Initialize a GPOptimizerProcess object.
+
+        Args:
+            num_params (int): The number of parameters.
+            num_repeats (int, optional): The number of repeats. Defaults to 1.
+            num_outputs (int, optional): The number of outputs. Defaults to 1.
+            **kwargs: Additional keyword arguments.
+
+        """
+        super().__init__(num_params, num_repeats, num_outputs, **kwargs)
