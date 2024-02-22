42de41042fd72da9d302f7634bccb17f5003f9c9
Shay Snyder <snyderse@outlook.com>
2024-02-21 21:14:45 -0500

    Add options for maximum iterations, number of repeats, and solver class

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import argparse
 from omegaconf import DictConfig
-import os
+from skopt.space import Space
 
-from hdbo.factory import function_factory, VALID_FUNCTIONS
+from hdbo.test_functions.processes import BaseFunctionProcess
+from hdbo.factory import function_factory, VALID_FUNCTIONS, VALID_SOLVERS
+from hdbo.solver import BOSolver
 
 DESCRIPTION = "Hyperdimensional Bayesian Optimization in Lava"
 
@@ -16,8 +18,27 @@ def get_config() -> DictConfig:
         "--function",
         type=str,
         choices=VALID_FUNCTIONS,
-        default="branin",
+        default="ackley",
         help="The function to optimize",
+    )
+    parser.add_argument(
+        "--max-iter",
+        type=int,
+        default=100,
+        help="The maximum number of iterations to run",
+    )
+    parser.add_argument(
+        "--num_repeats",
+        type=int,
+        default=1,
+        help="The number of times to repeat the optimization at every iteration",
+    )
+    parser.add_argument(
+        "--solver_class",
+        type=str,
+        default="vsa-cpu",
+        help="The class of solver to use",
+        choices=VALID_SOLVERS
     )
 
     args = parser.parse_args()
@@ -30,13 +51,25 @@ def get_config() -> DictConfig:
 
 def print_intro(config: DictConfig, delimiter="-", delimiter_width=60):
     """
-    TODO Finish Documentation
+    Print the introduction section of the program.
+
+    Args:
+        config (DictConfig): The configuration object containing program
+            settings.
+        delimiter (str, optional): The delimiter character to use for the
+            separator line. Defaults to "-".
+        delimiter_width (int, optional): The width of the separator line.
+            Defaults to 60.
     """
     print(delimiter * delimiter_width)
     print(DESCRIPTION)
     print(delimiter * delimiter_width)
     print(f"Configuration:")
     print(f" - Black Box Function: {config.function}")
+    print(f" - Max Iterations: {config.max_iter}")
+    print(f" - Number of Repeats: {config.num_repeats}")
+    print(f" - Solver Class: {config.solver_class}")
+    print(delimiter * delimiter_width)
 
 
 def main(config: DictConfig):
@@ -44,6 +77,10 @@ def main(config: DictConfig):
     TODO Finish Documentation
     """
     print_intro(config)
+
+    function_process, search_space, minima = function_factory(config.function)
+    solver = BOSolver(config)
+    solver.solve(function_process, search_space, minima)
 
 
 if __name__ == "__main__":
