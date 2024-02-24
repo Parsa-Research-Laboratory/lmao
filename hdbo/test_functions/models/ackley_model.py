91cdd84219d3c129248410fe2d4e9dcaf994f733
Shay Snyder <snyderse@outlook.com>
2024-02-23 21:24:55 -0500

    adding ackley model

---
+++ hdbo/test_functions/models/ackley_model.py
@@ -0,0 +1,24 @@
+from lava.magma.core.model.py.model import PyLoihiProcessModel
+from lava.magma.core.decorator import implements, requires, tag
+from lava.magma.core.resources import CPU
+from lava.magma.core.model.py.type import LavaPyType
+from lava.magma.core.model.py.ports import PyInPort, PyOutPort
+from lava.magma.core.sync.protocols.loihi_protocol import LoihiProtocol
+import numpy as np
+
+from ..processes.ackley_process import AckleyProcess
+
+
+@implements(proc=AckleyProcess, protocol=LoihiProtocol)
+@requires(CPU)
+@tag('floating_pt')
+class PyAckleyProcessModel(PyLoihiProcessModel):
+    input_port: PyInPort = LavaPyType(PyInPort.VEC_DENSE, np.float32)
+    output_port: PyOutPort = LavaPyType(PyOutPort.VEC_DENSE, np.float32)
+
+    num_params = LavaPyType(int, int)
+    num_outputs = LavaPyType(int, int)
+    num_repeats = LavaPyType(int, int)
+
+    def run_async(self):
+        print("Running Ackley P1")
